941e8b7b8c99ad73d3c14a52d4e50de2c0f1ebbd
Tudor Cismarescu <tudor.cismarescu@gmail.com>
2021-04-24 16:08:09 -0400

    Adds jumping (#33)

--- deeper-and-deeper/Assets/Prefabs/_Scripts/PlayerMovement.cs
+++ deeper-and-deeper/Assets/Prefabs/_Scripts/PlayerMovement.cs
@@ -6,17 +6,33 @@ public class PlayerMovement : MonoBehaviour {
     public float verticalSpeed;
 	public Rigidbody2D rb;
 
-    // Update is called once per frame
+	[SerializeField] private LayerMask platformLayerMask;
+	private BoxCollider2D boxCollider;
+
+	void Start() {
+		boxCollider = transform.GetComponent<BoxCollider2D>();
+	}
+
     void FixedUpdate() {
-		Vector2 horizontalVelocity = new Vector2(Input.GetAxis("Horizontal") * horizontalSpeed, 0);
 		Vector2 verticalVelocity = VerticalVelocity();
+		Vector2 horizontalVelocity = new Vector2(Input.GetAxis("Horizontal") * horizontalSpeed, 0f);
 		rb.velocity = horizontalVelocity + verticalVelocity;
     }
 
 	private Vector2 VerticalVelocity() {
 		// Clamp falling speed
 		float velocity = Mathf.Clamp(rb.velocity.y, -verticalSpeed, verticalSpeed);
-		// Get jump
-		return new Vector2(0, velocity);
+		if(Input.GetKeyDown(KeyCode.Space)) {
+			if(IsGrounded()) {
+				velocity = verticalSpeed;
+			}
+		}
+		return new Vector2(0f, velocity);
+	}
+
+	private bool IsGrounded() {
+		RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.down, .1f, platformLayerMask);
+		Debug.Log(hit.collider);
+		return hit.collider != null;
 	}
 }
